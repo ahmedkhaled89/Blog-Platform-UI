c35cb6c774116f2890a75eeaf0b6a4a39e310f83
ahmedkhaled <ahmedkhaled8901@gmail.com>
2024-09-07 23:40:18 +0300

    Add basic structure for Login page

---
+++ src/pages/users/Login.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import Alert from '../../Components/Alert';
+
+const Login = () => {
+  const [error, setError] = useState(null);
+
+  // Form Data State
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  // handle Login Function
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+
+  return (
+    <section className='card'>
+      <h1 className='title'>Login to your account</h1>
+      <form onSubmit={handleLogin}>
+        <input
+          type='email'
+          placeholder='Email Address'
+          className='input'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          autoFocus
+        />
+        <input
+          type='password'
+          placeholder='Password'
+          className='input'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button className='btn'>Login</button>
+      </form>
+      {error && <Alert msg={error} />}
+    </section>
+  );
+};
+
+export default Login;
