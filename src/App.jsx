22949ba0f8d80958e232db41036c17bee7ad3557
ahmedkhaled <ahmedkhaled8901@gmail.com>
2024-09-07 22:52:53 +0300

    Add Routes to App Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './pages/Layout';
 const App = () => {
-  return <>hi</>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
