94ad59606d315de973bb457601caad629c557f15
ahmedkhaled <ahmedkhaled8901@gmail.com>
2024-09-08 21:06:37 +0300

    Render user post in Dashboard

--- src/pages/users/Dashboard.jsx
+++ src/pages/users/Dashboard.jsx
@@ -1,5 +1,33 @@
+import { useContext, useEffect } from 'react';
+import { getUserPosts } from '../../Controllers/postsController';
+import { UserContext } from '../../contexts/UserContext';
+import Post from '../../Components/Post';
+
 const Dashboard = () => {
-  return <>USER DASHBOARD</>;
+  // Use User Contest
+  const { user, setUser } = useContext(UserContext);
+  useEffect(() => {
+    setTimeout(async () => {
+      // Grab user's posts
+      const { userPosts, email } = await getUserPosts();
+      // Update User State
+      setUser({ email, posts: userPosts });
+    }, 1000);
+  }, []);
+  console.log(user);
+
+  return (
+    <section className='card'>
+      <p>{user.email}</p>
+      <h1 className='title'>User Dashboard</h1>
+      {user.posts &&
+        user.posts.map((post) => (
+          <div key={post._id}>
+            <Post post={post}></Post>
+          </div>
+        ))}
+    </section>
+  );
 };
 
 export default Dashboard;
