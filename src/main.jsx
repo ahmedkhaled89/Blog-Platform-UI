f85bbbad9b18581f7cbaf9b6f30e63b7f43cb2bc
ahmedkhaled <ahmedkhaled8901@gmail.com>
2024-09-08 18:49:33 +0300

    Add PostContext

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './assets/app.css';
 import UserProvider from './contexts/UserContext.jsx';
+import PostProvider from './Contexts/PostContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <UserProvider>
-      <App />
-    </UserProvider>
+    <PostProvider>
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </PostProvider>
   </StrictMode>
 );
