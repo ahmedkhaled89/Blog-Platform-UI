3cdea8b78c377aa470a651ce7d860fe35ec4de54
ahmedkhaled <ahmedkhaled8901@gmail.com>
2024-09-08 14:56:36 +0300

    Add UserContext

---
+++ src/Contexts/UserContext.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/prop-types */
+import { useState, createContext } from 'react';
+
+export const UserContext = createContext();
+
+const UserProvider = ({ children }) => {
+  const [user, setUser] = useState({
+    email: localStorage.getItem('email'),
+    post: [],
+  });
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export default UserProvider;
