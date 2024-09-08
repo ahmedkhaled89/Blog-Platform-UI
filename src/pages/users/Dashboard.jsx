511db493b0c419b739b884c92b86f54add9b86f7
ahmedkhaled <ahmedkhaled8901@gmail.com>
2024-09-08 21:56:41 +0300

    Add delete, update and spinner to Dashboard

--- src/pages/users/Dashboard.jsx
+++ src/pages/users/Dashboard.jsx
@@ -1,29 +1,51 @@
-import { useContext, useEffect } from 'react';
-import { getUserPosts } from '../../Controllers/postsController';
+import { useContext, useEffect, useState } from 'react';
+import { deletePost, getUserPosts } from '../../Controllers/postsController';
 import { UserContext } from '../../contexts/UserContext';
 import Post from '../../Components/Post';
+import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
   // Use User Contest
   const { user, setUser } = useContext(UserContext);
+
+  // Loading State
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     setTimeout(async () => {
       // Grab user's posts
       const { userPosts, email } = await getUserPosts();
       // Update User State
       setUser({ email, posts: userPosts });
+      // Remove the loading
+      setLoading(false);
     }, 1000);
   }, []);
-  console.log(user);
 
   return (
     <section className='card'>
       <p>{user.email}</p>
       <h1 className='title'>User Dashboard</h1>
+      {loading && (
+        <i className='fa-solid fa-spinner animate-spin text-4xl text-center block'></i>
+      )}
       {user.posts &&
         user.posts.map((post) => (
           <div key={post._id}>
-            <Post post={post}></Post>
+            <Post post={post}>
+              <div className='flex items-center gap-2'>
+                <Link
+                  className='fa-solid fa-pen-to-square nav-link text-green-500 hover:bg-green-200'
+                  title='update'
+                  to='/update'
+                ></Link>
+                <button
+                  className='fa-solid fa-trash-can nav-link text-red-500 hover:bg-red-200'
+                  title='Delete'
+                  onClick={() => deletePost(post._id)}
+                ></button>
+              </div>
+            </Post>
           </div>
         ))}
     </section>
