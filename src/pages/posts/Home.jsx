e973b1366f986d76bae4845c4b06e3f8df032b9c
ahmedkhaled <ahmedkhaled8901@gmail.com>
2024-09-08 18:50:21 +0300

    Use PostContext to update posts

--- src/pages/posts/Home.jsx
+++ src/pages/posts/Home.jsx
@@ -1,18 +1,30 @@
-import { useEffect } from 'react';
+import { useContext, useEffect } from 'react';
 import { getPosts } from '../../Controllers/postsController';
+import { PostContext } from '../../Contexts/PostContext';
+import Post from '../../Components/Post';
 
 const Home = () => {
+  // Use post context
+  const { posts, setPosts } = useContext(PostContext);
   useEffect(() => {
     setTimeout(async () => {
       const data = await getPosts();
-      console.log(data.data);
+      // console.log(data.data.posts);
+      setPosts(data.data.posts);
     }, 3000);
   }, []);
+  console.log(posts);
+
   return (
     <section className='card'>
       <h1 className='title'>Latest Posts</h1>
 
-      <div>posts</div>
+      {posts &&
+        posts.map((post) => (
+          <div key={post._id}>
+            <Post post={post} />
+          </div>
+        ))}
     </section>
   );
 };
