2b1a5df93bc00b146a6306b0bcc09300e79bd076
ahmedkhaled <ahmedkhaled8901@gmail.com>
2024-09-10 20:05:54 +0300

    change header links dynamically based on userState

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,6 +1,23 @@
-import { Link, Outlet } from 'react-router-dom';
+import { useContext } from 'react';
+import { Link, Outlet, useNavigate } from 'react-router-dom';
+import { UserContext } from '../Contexts/UserContext';
 
 const Layout = () => {
+  const navigate = useNavigate();
+  const { user, setUser } = useContext(UserContext);
+
+  const handleLogout = () => {
+    if (confirm('Confirm Logout')) {
+      setUser({
+        email: null,
+        posts: [],
+      });
+      localStorage.removeItem('email');
+      localStorage.removeItem('token');
+      navigate('/');
+    }
+  };
+
   return (
     <>
       <header className='bg-indigo-500 text-white'>
@@ -10,18 +27,38 @@ const Layout = () => {
             to='/'
             className='fa-solid fa-house-chimney nav-link'
           ></Link>
-          <div className='flex items-center gap-2'>
-            <Link
-              title='Login'
-              to='/login'
-              className='fa-solid fa-right-to-bracket nav-link'
-            ></Link>
-            <Link
-              title='Register'
-              to='/register'
-              className='fa-solid fa-user-plus nav-link'
-            ></Link>
-          </div>
+          {user.email ? (
+            <div className='flex items-center gap-2'>
+              <Link
+                title='Create Post'
+                to='/create'
+                className='fa-solid fa-circle-plus nav-link'
+              ></Link>
+              <Link
+                title='Dashboard'
+                to='/dashboard'
+                className='fa-solid fa-circle-user nav-link'
+              ></Link>
+              <button
+                title='Logout'
+                onClick={handleLogout}
+                className='fa-solid fa-right-from-bracket nav-link'
+              ></button>
+            </div>
+          ) : (
+            <div className='flex items-center gap-2'>
+              <Link
+                title='Login'
+                to='/login'
+                className='fa-solid fa-right-to-bracket nav-link'
+              ></Link>
+              <Link
+                title='Register'
+                to='/register'
+                className='fa-solid fa-user-plus nav-link'
+              ></Link>
+            </div>
+          )}
         </nav>
       </header>
       <main className='p-4'>
